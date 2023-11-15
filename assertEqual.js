07f94292fc52470c1bc18ed7559fbf732a593f75
mariefranceperrier <mariefrance.perrier@gmail.com>
2023-11-15 15:56:45 -0500

    updated assertEqual function with template literals

--- assertEqual.js
+++ assertEqual.js
@@ -1,8 +1,8 @@
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log("🟢", "Assertion Passed:", actual, "===", expected);
+    console.log(`🟢 Assertion Passed: ${actual} === ${expected}`);
   } else if (actual !== expected) {
-    console.log("🔴", "Assertion failed:", actual, "!==", expected);
+    console.log(`🔴 Assertion failed: ${actual} !== ${expected}`);
   }
 };
 
