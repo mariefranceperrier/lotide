5304fb895ff3ff9e3a83b87130144f571a5ffcd5
mariefranceperrier <mariefrance.perrier@gmail.com>
2023-11-17 10:58:39 -0500

    correction post AI feedback

--- eqArrays.js
+++ eqArrays.js
@@ -12,8 +12,18 @@ Implement a function eqArrays which takes in two arrays and returns true or fals
 */
 
 const eqArrays = function (arr1, arr2) {
-  return JSON.stringify(arr1) === JSON.stringify(arr2);
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
 }
+
 
 eqArrays([1, 2, 3], [1, 2, 3]) // => true
 eqArrays([1, 2, 3], [3, 2, 1]) // => false
