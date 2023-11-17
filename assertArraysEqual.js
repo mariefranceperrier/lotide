adacd449ec757868cc016a8f47088c29e6e6ee9e
mariefranceperrier <mariefrance.perrier@gmail.com>
2023-11-17 11:48:48 -0500

    correction post AI feedback

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -14,12 +14,13 @@ const eqArrays = function (arr1, arr2) {
 // Implement assertArraysEqual which will take in two arrays and console.log an appropriate message to the console.
 
 const assertArraysEqual = function (actualArr, expectedArr) {
-  if (!eqArrays) {
+  if (!eqArrays(actualArr, expectedArr)) {
     console.log(`🔴 Assertion failed: ${actualArr} !== ${expectedArr}`);
   } else {
     console.log(`🟢 Assertion Passed: ${actualArr} === ${expectedArr}`);
   }
 };
+
 
 eqArrays([1, 2, 3], [1, 2, 3]) // => true
 eqArrays([1, 2, 3], [3, 2, 1]) // => false
@@ -27,7 +28,7 @@ eqArrays([1, 2, 3], [3, 2, 1]) // => false
 eqArrays(["1", "2", "3"], ["1", "2", "3"]) // => true
 eqArrays(["1", "2", "3"], ["1", "2", 3]) // => false
 
-assertArraysEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertArraysEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
-assertArraysEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
-assertArraysEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
+assertArraysEqual([1, 2, 3], [1, 2, 3]);
+assertArraysEqual([1, 2, 3], [3, 2, 1]);
+assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]);
+assertArraysEqual(["1", "2", "3"], ["1", "2", 3]);
