be5cc70876e2efcae36f9f149a05fd6d72929846
mariefranceperrier <mariefrance.perrier@gmail.com>
2023-12-05 09:39:00 -0500

    refactoring: module.exports

--- assertEqual.js
+++ assertEqual.js
@@ -6,8 +6,5 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual("ONE", "ONE");
-assertEqual("HI", "BYE");
-assertEqual(1, 111);
+
+module.exports = assertEqual;
