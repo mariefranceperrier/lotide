506547d8e0edaaaecf8f6685c73cc2389e7f2791
mariefranceperrier <mariefrance.perrier@gmail.com>
2023-12-05 10:12:21 -0500

    refactoring: exporting test code to eqArraysTest.js

--- eqArrays.js
+++ eqArrays.js
@@ -1,16 +1,3 @@
-const assertEqual = function (actual, expected) {
-  if (actual === expected) {
-    console.log(`🟢 Assertion Passed: ${actual} === ${expected}`);
-  } else { 
-    console.log(`🔴 Assertion failed: ${actual} !== ${expected}`);
-  }
-};
-
-
-/*Instruction
-Implement a function eqArrays which takes in two arrays and returns true or false, based on a perfect match.
-*/
-
 const eqArrays = function (arr1, arr2) {
   if (arr1.length !== arr2.length) {
     return false;
@@ -31,7 +18,5 @@ eqArrays([1, 2, 3], [3, 2, 1]) // => false
 eqArrays(["1", "2", "3"], ["1", "2", "3"]) // => true
 eqArrays(["1", "2", "3"], ["1", "2", 3]) // => false
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
+
+module.exports = eqArrays;
