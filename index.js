3493969a92864248c6c40d6aeab371ae13a33e06
mariefranceperrier <mariefrance.perrier@gmail.com>
2023-12-05 16:14:46 -0500

    removed map, did  regrouping

--- index.js
+++ index.js
@@ -1,42 +1,46 @@
+const assertEqual = require('./assertEqual');
+
 const head = require('./head');
 const tail = require('./tail');
-const middle = require('./middle');
-const countLetters = require('./countLetters');
-const countOnly = require('./countOnly');
-const findKey = require('./findKey');
-const findKeyByValue = require('./findKeyByValue');
-const flatten = require('./flatten');
-const letterPositions = require('./letterPositions');
-const map = require('./map');
-const takeUntil = require('./takeUntil');
-const without = require('./without');
 
-const eqObjects = require('./eqObjects');
 const eqArrays = require('./eqArrays');
-
-const assertEqual = require('./assertEqual');
-const assertObjectsEqual = require('./assertObjectsEqual');
 const assertArraysEqual = require('./assertArraysEqual');
+const without = require('./without');
+const flatten = require('./flatten');
+const middle = require('./middle');
+
+const countOnly = require('./countOnly');
+const countLetters = require('./countLetters');
+const letterPositions = require('./letterPositions');
+const findKeyByValue = require('./findKeyByValue');
+const eqObjects = require('./eqObjects');
+const assertObjectsEqual = require('./assertObjectsEqual');
+
+
+const takeUntil = require('./takeUntil');
+const findKey = require('./findKey');
 
 
 module.exports = {
+  assertEqual: assertEqual,
+  
   head: head,
   tail: tail,
-  middle: middle,
-  countLetters: countLetters,
-  countOnly: countOnly,
-  findKey: findKey,
-  findKeyByValue: findKeyByValue,
-  flatten: flatten,
-  letterPositions: letterPositions,
-  map: map,
-  takeUntil: takeUntil,
-  without: without,
-
-  eqObjects: eqObjects,
+  
   eqArrays: eqArrays,
-
-  assertEqual: assertEqual,
+  assertArraysEqual: assertArraysEqual,
+  without: without,
+  flatten: flatten,
+  middle: middle,
+  
+  countOnly: countOnly,
+  countLetters: countLetters,
+  letterPositions: letterPositions,
+  findKeyByValue: findKeyByValue,
+  eqObjects: eqObjects,
   assertObjectsEqual: assertObjectsEqual,
-  assertArraysEqual: assertArraysEqual
+  
+  takeUntil: takeUntil,
+  findKey: findKey
+
 };
