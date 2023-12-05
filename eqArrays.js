004b0b7beca030caba4f2c5af8af394b6dc06265
mariefranceperrier <mariefrance.perrier@gmail.com>
2023-12-05 10:15:59 -0500

    dry: removed test cases

--- eqArrays.js
+++ eqArrays.js
@@ -12,11 +12,4 @@ const eqArrays = function (arr1, arr2) {
 }
 
 
-eqArrays([1, 2, 3], [1, 2, 3]) // => true
-eqArrays([1, 2, 3], [3, 2, 1]) // => false
-
-eqArrays(["1", "2", "3"], ["1", "2", "3"]) // => true
-eqArrays(["1", "2", "3"], ["1", "2", 3]) // => false
-
-
 module.exports = eqArrays;
