d407c9b311d013db5fd26d88709f53691b841a1e
mariefranceperrier <mariefrance.perrier@gmail.com>
2023-12-05 09:40:00 -0500

    added file assertEqualTest.js

---
+++ test/assertEqualTest.js
@@ -0,0 +1,10 @@
+const assertEqual = require('../assertEqual');
+
+
+// TEST CODE for assertEqual.js
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("ONE", "ONE");
+assertEqual("HI", "BYE");
+assertEqual(1, 111);
