8b903127c8afcac461acba8b43b257e592ab2524
mariefranceperrier <mariefrance.perrier@gmail.com>
2023-12-05 15:10:45 -0500

    added a test with only 1 element in array

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,13 +1,16 @@
 const assert = require('chai').assert;
 const tail = require('../tail');
 const words = ['Yo Yo', 'Lighthouse', 'Labs'];
+const str = ['Hey'];
 
 
 describe("#tail", () => {
   it("returns ['Lighthouse', 'Labs'] for ['Yo Yo', 'Lighthouse', 'Labs']", () => {
     assert.deepEqual(tail(words), ['Lighthouse', 'Labs']);
   });
-
+  it("returns [] for ['Hey']", () => {
+    assert.deepEqual(tail(str), []);
+  });
   it("makes sure the original array is not altered by the tail function", () => {
     tail(words);
     assert.strictEqual(words.length, 3)
