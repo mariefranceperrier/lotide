5d165136d720e5dc2eb98175564efd0585778f45
mariefranceperrier <mariefrance.perrier@gmail.com>
2023-12-05 10:37:40 -0500

    refactoring: require eqArrays +  export asserArraysEqual

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,17 +1,4 @@
-const eqArrays = function (arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  return true;
-}
-
-// Implement assertArraysEqual which will take in two arrays and console.log an appropriate message to the console.
+const eqArrays = require('./eqArrays');
 
 const assertArraysEqual = function (actualArr, expectedArr) {
   if (!eqArrays(actualArr, expectedArr)) {
@@ -21,7 +8,5 @@ const assertArraysEqual = function (actualArr, expectedArr) {
   }
 };
 
-assertArraysEqual([1, 2, 3], [1, 2, 3]);
-assertArraysEqual([1, 2, 3], [3, 2, 1]);
-assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]);
-assertArraysEqual(["1", "2", "3"], ["1", "2", 3]);
+
+module.exports = assertArraysEqual;
