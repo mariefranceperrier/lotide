7d1d20780c209d89157a5db0d6755a5952098295
mariefranceperrier <mariefrance.perrier@gmail.com>
2023-12-05 09:46:16 -0500

    dry up: took out else if

--- assertEqual.js
+++ assertEqual.js
@@ -1,7 +1,7 @@
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
     console.log(`🟢 Assertion Passed: ${actual} === ${expected}`);
-  } else if (actual !== expected) {
+  } else {
     console.log(`🔴 Assertion failed: ${actual} !== ${expected}`);
   }
 };
