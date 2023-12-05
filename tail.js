f382ad4f56fe5bf30c5108fc9931726da523de8b
mariefranceperrier <mariefrance.perrier@gmail.com>
2023-12-05 09:58:38 -0500

    refactoring: exporting test code to tailTest.js

--- tail.js
+++ tail.js
@@ -1,25 +1,5 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`🟢 Assertion Passed: ${actual} === ${expected}`);
-  } else if (actual !== expected) {
-    console.log(`🔴 Assertion failed: ${actual} !== ${expected}`);
-  }
-};
-
-
-
 const tail = function(arr) {
   return arr.slice(1);
 };
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-const test = ["Hey"];
-const testEle = tail(test);
-const result = tail(words);
-
-assertEqual(words.length, 3);
-assertEqual(result.length, 2);
-assertEqual(result[0], "Lighthouse");
-assertEqual(result[1], "Labs");
-assertEqual(testEle.length, 0);
-assertEqual(tail([]).length, 0);
+module.exports = tail;
