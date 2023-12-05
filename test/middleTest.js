5ca75f85083db5a76ff8e48a03db3502db9a737e
mariefranceperrier <mariefrance.perrier@gmail.com>
2023-12-05 12:37:50 -0500

    added test to ensure original array intact

--- test/middleTest.js
+++ test/middleTest.js
@@ -5,14 +5,16 @@ describe("#middle", () => {
   it("returns [3, 4] for [1, 2, 3, 4, 5, 6]", () => {
     assert.deepEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
   });
-  it("returns [3] for [1, 2, 3, 4, 5]", () => {
-    assert.deepEqual(middle([1, 2, 3, 4, 5]), [3]);
-  });
   it("returns [2] for [1, 2, 3]", () => {
     assert.deepEqual(middle([1, 2, 3]), [2]);
   });
   it("returns [] for [1]", () => {
     assert.deepEqual(middle([1]), []);
+  });
+  it("makes sure the original array is not altered by the middle function", () => {
+    const words = ['hello', 'world', 'lighthouse'];
+    middle(words);
+    assert.strictEqual(words.length, 3);
   });
 
 });
