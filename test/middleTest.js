c5c72fdc39ea2502214ddf2fb89a73ffdae8e75e
mariefranceperrier <mariefrance.perrier@gmail.com>
2023-12-05 12:29:40 -0500

    added more test codes

--- test/middleTest.js
+++ test/middleTest.js
@@ -8,4 +8,11 @@ describe("#middle", () => {
   it("returns [3] for [1, 2, 3, 4, 5]", () => {
     assert.deepEqual(middle([1, 2, 3, 4, 5]), [3]);
   });
+  it("returns [2] for [1, 2, 3]", () => {
+    assert.deepEqual(middle([1, 2, 3]), [2]);
+  });
+  it("returns [] for [1]", () => {
+    assert.deepEqual(middle([1]), []);
+  });
+
 });
