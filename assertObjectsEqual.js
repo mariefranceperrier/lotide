7d992908d50e1cf9185929567f4bc12dc1439455
mariefranceperrier <mariefrance.perrier@gmail.com>
2023-12-08 15:19:12 -0500

    dry: require eqArrays

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -1,14 +1,4 @@
-const eqArrays = function (arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  return true;
-}
+const eqArrays = require('./eqArrays');
 
 
 const eqObjects = function (object1, object2) {
