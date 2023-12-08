5bd81336cc709408246287b13ee9531b9640800a
mariefranceperrier <mariefrance.perrier@gmail.com>
2023-12-08 10:43:07 -0500

    added file letterPositionsTest.js

---
+++ test/letterPositionsTest.js
@@ -0,0 +1,10 @@
+const eqArrays = require('../eqArrays');
+const assertArraysEqual = require('../assertArraysEqual');
+const letterPositions = require('../letterPositions');
+
+const result1 = letterPositions("hello");
+
+assertArraysEqual(result1["h"], [0]);
+assertArraysEqual(result1["e"], [1]);
+assertArraysEqual(result1["l"], [2, 3]);
+assertArraysEqual(result1["o"], [4]);
