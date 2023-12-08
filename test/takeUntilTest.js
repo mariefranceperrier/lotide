8885cef5723353255d21fdebf221088926ee8399
mariefranceperrier <mariefrance.perrier@gmail.com>
2023-12-08 14:54:39 -0500

    added file takeUntilTest.js

---
+++ test/takeUntilTest.js
@@ -0,0 +1,19 @@
+const assert = require('chai').assert;
+const takeUntil = require('../takeUntil');
+
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, x => x < 0);
+
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+const results2 = takeUntil(data2, x => x === ',');
+
+
+describe("#takeUntil", () => {
+  it("returns [1, 2, 5, 7, 2] for [1, 2, 5, 7, 2, -1, 2, 4, 5]", () => {
+    assert.deepEqual(results1, [1, 2, 5, 7, 2]);
+  });
+  it("returns ['I've', 'been', 'to', 'Hollywood'] for ['I/'ve', 'been', 'to', 'Hollywood', ',', 'I/'ve', 'been', 'to', 'Redwood']", () => {
+    assert.deepEqual(results2, ["I've", 'been', 'to', 'Hollywood']);
+  });
+});
+
