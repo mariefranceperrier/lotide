38e6ba0f329da4131905efc92ab6f58ad8735b3f
mariefranceperrier <mariefrance.perrier@gmail.com>
2023-12-08 15:56:23 -0500

    dry: exporting test code to takeUntilTest.js

--- takeUntil.js
+++ takeUntil.js
@@ -1,32 +1,5 @@
-const eqArrays = function (arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  return true;
-}
-
-
-const assertArraysEqual = function (actualArr, expectedArr) {
-  if (!eqArrays(actualArr, expectedArr)) {
-    console.log(`🔴 Assertion failed: ${actualArr} !== ${expectedArr}`);
-  } else {
-    console.log(`🟢 Assertion Passed: ${actualArr} === ${expectedArr}`);
-  }
-};
-
-// 1.We will be implementing a function quite similar to this one, but isn't present in their library: takeUntil.
-// 2. It will take in two parameters an array and a callback 
-// 3. The function will return a "slice of the array with elements taken from the beginning." 
-// 4. It should keep going until the callback / predicate returns a truthy value.
-
 const takeUntil = function (array, callback) {
   
-
   let result = [];
   for (const item of array) {
     if (callback(item)) {
@@ -37,13 +10,4 @@ const takeUntil = function (array, callback) {
   return result;
 }
 
-const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
-const results1 = takeUntil(data1, x => x < 0);
-console.log(results1);
-assertArraysEqual(results1, [1, 2, 5, 7, 2]);
-
-
-const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
-const results2 = takeUntil(data2, x => x === ',');
-console.log(results2);
-assertArraysEqual(results2, ["I've", 'been', 'to', 'Hollywood']);
+module.exports = takeUntil;
