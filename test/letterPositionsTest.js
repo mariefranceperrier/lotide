d384e6a183568e71f0c890a3e09498baec774a7f
mariefranceperrier <mariefrance.perrier@gmail.com>
2023-12-08 14:25:43 -0500

    conversion to Mocha and Chai test code

--- test/letterPositionsTest.js
+++ test/letterPositionsTest.js
@@ -1,10 +1,17 @@
-const eqArrays = require('../eqArrays');
-const assertArraysEqual = require('../assertArraysEqual');
+const assert = require('chai').assert;
 const letterPositions = require('../letterPositions');
+
 
 const result1 = letterPositions("hello");
 
-assertArraysEqual(result1["h"], [0]);
-assertArraysEqual(result1["e"], [1]);
-assertArraysEqual(result1["l"], [2, 3]);
-assertArraysEqual(result1["o"], [4]);
+describe("#letterPositionsTest", () => {
+  it("returns index [0] for position of letter ['h']", () => {
+    assert.deepEqual(result1['h'], [0]);
+  });
+  it("returns index [2, 3] for position of letter ['l']", () => {
+    assert.deepEqual(result1['l'], [2, 3]);
+  });
+  it("returns index [4] for position of letter ['o']", () => {
+    assert.deepEqual(result1['o'], [4]);
+  });
+});
