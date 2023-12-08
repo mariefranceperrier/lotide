f9f75b79285d8ebb529cc5dd8e1dc86e3ea116f2
mariefranceperrier <mariefrance.perrier@gmail.com>
2023-12-08 10:18:21 -0500

    dry: require assertEqual + export test code

--- countOnly.js
+++ countOnly.js
@@ -1,10 +1,4 @@
-const assertEqual = function (actual, expected) {
-  if (actual === expected) {
-    console.log(`🟢 Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🔴 Assertion failed: ${actual} !== ${expected}`);
-  }
-};
+const assertEqual = require('./assertEqual');
 
 const countOnly = function (allItems, itemsToCount) {
   const results = {};
@@ -21,24 +15,4 @@ const countOnly = function (allItems, itemsToCount) {
 }
 
 
-// TEST CODE
-
-const firstNames = [
-  "Karl",
-  "Salima",
-  "Agouhanna",
-  "Fang",
-  "kavith",
-  "Jason",
-  "Salima",
-  "Fang",
-  "Joe"
-];
-
-const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
-
-
-assertEqual(result1["Jason"], 1);
-assertEqual(result1["Karima"], undefined);
-assertEqual(result1["Fang"], 2);
-assertEqual(result1["Agouhanna"], undefined);
+module.exports = countOnly;
