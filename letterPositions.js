4774be43a05fa9e9c32df9e5aa5d4053549fb118
mariefranceperrier <mariefrance.perrier@gmail.com>
2023-12-08 10:46:49 -0500

    dry: exported test codes to letterPositionsTest.js

--- letterPositions.js
+++ letterPositions.js
@@ -1,4 +1,3 @@
-
 const letterPositions = function (sentence) {
   const results = {};
   
@@ -20,33 +19,4 @@ const letterPositions = function (sentence) {
 console.log(letterPositions("hello"));
 
 
-// TEST CODE
-
-const eqArrays = function (arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  return true;
-}
-
-const assertArraysEqual = function (actualArr, expectedArr) {
-  if (!eqArrays(actualArr, expectedArr)) {
-    console.log(`🔴 Assertion failed: ${actualArr} !== ${expectedArr}`);
-  } else {
-    console.log(`🟢 Assertion Passed: ${actualArr} === ${expectedArr}`);
-  }
-};
-
-
-const result1 = letterPositions("hello");
-
-assertArraysEqual(result1["h"], [0]);
-assertArraysEqual(result1["e"], [1]);
-assertArraysEqual(result1["l"], [2, 3]);
-assertArraysEqual(result1["o"], [4]);
+module.exports = letterPositions;
