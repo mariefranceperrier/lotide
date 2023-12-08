89ea7eb469aa57fcd589dbedf47886e13de236c6
mariefranceperrier <mariefrance.perrier@gmail.com>
2023-12-08 10:55:14 -0500

    dry: exported test code to findKeyByValueTest.js

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -1,12 +1,3 @@
-const assertEqual = function (actual, expected) {
-  if (actual === expected) {
-    console.log(`🟢 Assertion Passed: ${actual} === ${expected}`);
-  } else if (actual !== expected) {
-    console.log(`🔴 Assertion failed: ${actual} !== ${expected}`);
-  }
-};
-
-
 const findKeyByValue = function (obj, value) {
   for (const key in obj) {
     if (obj[key] === value) {
@@ -15,15 +6,5 @@ const findKeyByValue = function (obj, value) {
   }
 }
 
-// test code
 
-const bestTVShowsByGenre = {
-  sci_fi: "The Expanse",
-  comedy: "Brooklyn Nine-Nine",
-  drama: "The Wire"
-};
-
-console.log(findKeyByValue(bestTVShowsByGenre, "The Wire"));
-
-assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
-assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+module.exports = findKeyByValue;
