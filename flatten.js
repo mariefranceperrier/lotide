e2144a68fa61791b942a4cab37500853eddd604d
mariefranceperrier <mariefrance.perrier@gmail.com>
2023-12-08 08:58:34 -0500

    dry: require erArrays

--- flatten.js
+++ flatten.js
@@ -1,20 +1,4 @@
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-
-
-// 1. Create a function flatten which will take in an array containing elements including nested arrays of elements, 
-// 2. and return a "flattened" version of the array.
-// 3. Our function will  only handle one level of nesting.
+const eqArrays = require('./eqArrays');
 
 const flatten = function(arr) {
   const flattenedArr = [];
@@ -32,14 +16,7 @@ const flatten = function(arr) {
 };
 
 
-const assertArraysEqual = function(actualArr, expectedArr) {
-  if (!eqArrays(actualArr, expectedArr)) {
-    console.log(`🔴 Assertion failed: ${actualArr} !== ${expectedArr}`);
-  } else {
-    console.log(`🟢 Assertion Passed: ${actualArr} === ${expectedArr}`);
-  }
-};
-
 console.log(flatten([1, 2, [3, 4], 5, [6]]));
 
-assertArraysEqual(flatten([1, 2, [3, 4], 5, [6]]), [1, 2, 3, 4, 5, 6]);
+
+module.exports = flatten;
