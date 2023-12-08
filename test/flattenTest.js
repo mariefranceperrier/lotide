2dde0f4fb6f106b9ebe055ad5608bbd15fff236d
mariefranceperrier <mariefrance.perrier@gmail.com>
2023-12-08 11:26:55 -0500

    conversion to mocha and chai test code

--- test/flattenTest.js
+++ test/flattenTest.js
@@ -1,4 +1,11 @@
-const assertArraysEqual = require('../assertArraysEqual')
+const assert = require('chai').assert;
 const flatten = require('../flatten');
 
-assertArraysEqual(flatten([1, 2, [3, 4], 5, [6]]), [1, 2, 3, 4, 5, 6]);
+describe("#flatten", () => {
+  it("returns [1, 2, 3, 4, 5, 6] for [1, 2, [3, 4], 5, [6]]", () => {
+    assert.deepEqual(flatten([1, 2, [3, 4], 5, [6]]), [1, 2, 3, 4, 5, 6]);
+  });
+});
+
+
+
